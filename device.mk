e3f54e3a56150c4624392fa846b8dfa60444854f
Ethan Chen <intervigil@gmail.com>
2017-11-27 20:49:11 -0800

    Revert "urd: Use Lineage legacy camera provider"
    This reverts commit e384ccf2a8caf89157ff0b6c259488152ecefbc2.
    
    Change-Id: I16da7cbe24b627d3ce78f316d8be469337c2a277

--- device.mk
+++ device.mk
@@ -109,7 +109,7 @@ PRODUCT_PACKAGES += \
 # TODO
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
     camera.msm8952 \
     Snap
 
