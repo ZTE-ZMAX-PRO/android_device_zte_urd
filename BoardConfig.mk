6a74592e2db02cfb3500293c30a31a210c537de6
Armando <armandopartida0@gmail.com>
2018-08-12 17:13:27 -0400

    Moved ignore from device to board

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,6 +110,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
+# Keylayout
+PRODUCT_COPY_FILES := $(filter-out frameworks/base/data/keyboards/qwerty.kl:system/usr/keylayout/qwerty.kl, $(PRODUCT_COPY_FILES))
+
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
 
--- device.mk
+++ device.mk
@@ -135,8 +135,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/syna-touchscreen.idc:system/usr/idc/syna-touchscreen.idc
     
 # Keylayout
-PRODUCT_COPY_FILES := $(filter-out frameworks/base/data/keyboards/qwerty.kl:system/usr/keylayout/qwerty.kl, $(PRODUCT_COPY_FILES))
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
     $(LOCAL_PATH)/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl
