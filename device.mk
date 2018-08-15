7f8080c7e863e1f233ad7827c6f235c5b2e40f77
LuK1337 <priv.luk@gmail.com>
2018-08-15 15:48:09 +0200

    urd: camera: Add missing Fence destructor
    * Our camera HAL depends on it.
    
    Change-Id: Iedf3dae40e3f2beb284549f486b8721c69837cd0

--- device.mk
+++ device.mk
@@ -107,6 +107,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.msm8952 \
+    libshim_camera \
     Snap
 
 # Charger
--- shims/Android.mk
+++ shims/Android.mk
@@ -3,6 +3,20 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
+    Fence.cpp
+
+LOCAL_MODULE := libshim_camera
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+
+LOCAL_32_BIT_ONLY := true
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
   gps_shim.cpp
 
 LOCAL_MODULE := libshim_gps
