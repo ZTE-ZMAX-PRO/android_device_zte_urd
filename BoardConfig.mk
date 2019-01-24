7dafd65880ecafa1e679d19abaa270b95a155ce2
Armando Partida <armandopartida0@gmail.com>
2019-01-24 16:25:01 -0500

    Changed defconfig name and enabled permissive mode

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,7 +49,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk ramoops.mem_address=0x9fd00000 ramoops.mem_size=0x200000 ramoops.console_size=0x100000
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x01000000
@@ -58,7 +58,7 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/zte/urd
-TARGET_KERNEL_CONFIG := lineage_urd_defconfig
+TARGET_KERNEL_CONFIG := msm-urd_defconfig
 #TARGET_USES_UNCOMPRESSED_KERNEL := true
 
 # Audio
