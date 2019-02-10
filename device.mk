1fc02c1f0d48ad72f43034b3e296355aeecc848a
Armando Partida <armandopartida0@gmail.com>
2019-02-09 20:19:32 -0500

    Revert "biometrics: urd -> custom"
    This reverts commit 4c87f0c3181a9e72cb24afe60c239611dedcf06b.

--- device.mk
+++ device.mk
@@ -141,7 +141,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service-custom
+    android.hardware.biometrics.fingerprint@2.1-service.urd
 
 # Gesture Handler
 PRODUCT_PACKAGES += \
