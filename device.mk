bdd996e891ac6e117fbdb1e530e35d893cc5394d
Armando Partida <armandopartida0@gmail.com>
2019-03-24 18:00:57 -0400

    Added DRM support

--- device.mk
+++ device.mk
@@ -137,6 +137,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Doze
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.0-service.urd
