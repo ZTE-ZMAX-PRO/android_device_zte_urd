739dcc1e74ee47af70e01417c186cab1f3ae4900
Armando Partida <armandopartida0@gmail.com>
2019-04-19 09:34:18 -0400

    urd: config.fs: Add /firmware and /persist

--- config.fs
+++ config.fs
@@ -6,3 +6,15 @@ value:3012
 
 [AID_VENDOR_RFS_SHARED]
 value:3013
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
