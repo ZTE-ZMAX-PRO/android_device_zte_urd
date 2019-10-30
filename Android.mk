d9c873a6d233806f3692e59ea4002476b9a97c27
Armando Partida Sanabia <armandopartida0@gmail.com>
2019-10-30 11:51:30 -0400

    urd: Fix overrides

--- Android.mk
+++ Android.mk
@@ -21,13 +21,6 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
-WCNSS_CFG_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
-$(WCNSS_CFG_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf /data/vendor/wifi/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_CFG_SYMLINK)
-
 # RFS symlinks
 RFS_SYMLINKS := $(TARGET_OUT)/rfs
 $(RFS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
