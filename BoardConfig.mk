3cbe4736a909ab71dd91ead6b3dea2905643d39c
Armando Partida Sanabia <armandopartida0@gmail.com>
2019-11-19 13:22:00 +0000

    urd: ramdisk: Fix fstab path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,7 +125,7 @@ BOARD_USES_QC_TIME_SERVICES := true
 TARGET_USE_SDCLANG := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.qcom
 
 # RIL
 TARGET_RIL_VARIANT := caf
